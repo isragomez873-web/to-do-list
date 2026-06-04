02ace0f2b1efb240d70550aa018cd5d2b9c29b84
isragomez873-web <isragomez873@gmail.com>
2026-06-04 13:49:36 -0600

    ok

--- to-do-list-pwa/service-worker.js
+++ to-do-list-pwa/service-worker.js
@@ -1,12 +1,12 @@
 const cacheName = 'to-do-list-cache-v1';
 const archivos = [
-  '/to-do-list-pwa/',
-  '/to-do-list-pwa/index.html',
-  '/to-do-list-pwa/styles.css',
-  '/to-do-list-pwa/script.js',
-  '/to-do-list-pwa/manifest.json',
-  '/to-do-list-pwa/img/hadestdl.png',
-  '/to-do-list-pwa/img/hadestdl512.png'
+  '/to-do-list/to-do-list-pwa/',
+  '/to-do-list/to-do-list-pwa/index.html',
+  '/to-do-list/to-do-list-pwa/styles.css',
+  '/to-do-list/to-do-list-pwa/script.js',
+  '/to-do-list/to-do-list-pwa/manifest.json',
+  '/to-do-list/to-do-list-pwa/img/hadestdl.png',
+  '/to-do-list/to-do-list-pwa/img/hadestdl512.png'
 ];
 
 self.addEventListener('install', (event) => {
